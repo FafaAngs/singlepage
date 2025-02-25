0dfd8904150e3088c915781901b336769feab2db
Angs <118002169+FafaAngs@users.noreply.github.com>
2025-02-25 23:34:23 +0800

    Add files via upload

---
+++ scriptphoneversion.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+        document.querySelector(".nav-menu").classList.toggle("active");
+    }
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 50, 
+                behavior: 'smooth'
+            });
+        }
+    
+        document.querySelector(".nav-menu").classList.remove("active");
+    });
+});
+
+
+
+
+
+
+
+    
